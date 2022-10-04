dd064a6c668f2da91bcb5f08d479b9bff182aa1b
Kyu Sung Sohn <rok.ksohn@gmail.com>
2022-10-04 21:33:45 +0900

    22.10.04 T

---
+++ Programmers/Level 2/p_N개의 최소공배수.js
@@ -0,0 +1,17 @@
+//can be found at: https://school.programmers.co.kr/learn/courses/30/lessons/12953
+
+function solution(arr) {
+  let max = Math.max(...arr);
+  let count = 0, x = 0;
+
+  while(true){
+    x += max;
+
+    for(let i = 0; i < arr.length; i++) {
+      if(x % arr[i] !== 0) break; 
+      if(i === arr.length - 1 && x % arr[i] === 0) return x;
+    }
+  }
+}
+
+// console.log(solution([2,6,8,14]));
