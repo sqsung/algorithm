47444e8eae0399aa05ccf94278efc6a498fc58cf
Kyu Sung Sohn <rok.ksohn@gmail.com>
2022-10-12 13:09:28 +0900

    22.10.12 W

--- Programmers/Level 2/p_괄호 회전하기.js
+++ Programmers/Level 2/p_괄호 회전하기.js
@@ -1,3 +1,5 @@
+//problem at: https://school.programmers.co.kr/learn/courses/30/lessons/76502
+
 function parenthCheck(arr) {
     let temp = [];
 
---
+++ Programmers/Level 2/p_튜플.js
@@ -0,0 +1,42 @@
+//problem at: https://school.programmers.co.kr/learn/courses/30/lessons/64065
+
+function solution(s) {
+    let temp = [], result = [], answer = [], tempStr = '';
+
+    for(let i = 1; i < s.length - 1; i++) {
+        if(s[i] === '{' || s[i] === ' ') continue;
+        
+        if(s[i] === '}') {
+            result.push(temp);
+            temp = [];
+        }
+        if(/\d/.test(s[i]) === true && (s[i + 1] !== ',' || s[i + 1] !== '}')) tempStr += s[i];
+
+        if(/\d/.test(s[i]) === true && (s[i + 1] === ',' || s[i + 1] === '}')) {
+            temp.push(tempStr);
+            tempStr = '';
+        }
+    }
+
+    result.sort((a, b) => a.length - b.length);
+    
+    for(let i = 0; i < result.length; i++) {
+        for(let j = 0; j < result[i].length; j++) {
+            if(i === 0) answer.push(Number(result[0][0]));
+            else {
+                if(answer.includes(Number(result[i][j]))) continue;
+                else answer.push(Number(result[i][j]));
+            }
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution("{{20,111},{111}}"));
+console.log(solution("{{123}}"));
+// console.log(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"));
+
+/*
+"{{1,2,3},{2,1},{1,2,4,3},{2}}"	
+*/
